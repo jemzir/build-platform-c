eb44d0582f5ccbf6d813176ccb412e73ebc13a9d
jemzir <nghiemj23@gmail.com>
2023-01-15 13:08:24 -0800

    set up beginning outline of server and website landing page

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+const fs = require('fs');
+
+// git clone path/to/upstream/repo with any arbitrary git repo to a folder here... 
+  // the folder should hold differnt multiple independent c programs
+
+
+// use fs for traversal of the git repo for reading/writing
+// middleware for using make in terminal
+
+
+// send the responses to the website
+// throttle the responses and keep a cache of latest copies?
+  // periodically fetch the commits
+
+
+// serve the website
+
+// 
