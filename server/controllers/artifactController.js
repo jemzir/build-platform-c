e1a7a1c920e420fc49d5edb1acfa6eb4dab6fd1d
jemzir <nghiemj23@gmail.com>
2023-01-15 21:15:22 -0800

    add in shelljs to exec command lines when registering

--- server/controllers/artifactController.js
+++ server/controllers/artifactController.js
@@ -1,3 +1,5 @@
+const shell = require('shelljs');
+
 module.exports = {
   registerRepo: (req, res, next) => {
 // git clone path/to/upstream/repo with any arbitrary git repo to a folder here... 
@@ -5,7 +7,8 @@ module.exports = {
   // git clone <repo to clone> <location ending with directory name you want the repo under>
   // user should clone this build-platform and then cd into the repo folder:
 
-
+  // parse the body and use the path to do the git clone path
+  // use shelljs to execute git clone repoPath/url
   },
 
   checkRepo: (req, res, next) => {
--- server/server.js
+++ server/server.js
@@ -4,13 +4,18 @@ const PORT = 5000;
 
 const fs = require('fs');
 const path = require('path');
+const controller = require('./controllers/artifactController');
 
+// serve the initial landing page
 app.get('/', (req, res) => {
   res.status(200).sendFile(path.resolve(__dirname, '../views/index.html'));
 });
 
 // path for registering the repository
   // middleware: 'api', check if the directory name already exists, registering directory middleware, end
+app.post('/api', controller.checkRepo, controller.registerRepo, (req, res) => {
+  res.status(200).json() // send the result back to the frontend req
+})
 
 // use fs for traversal of the git repo for reading/writing --> look for the git-repo
   // can add in ability to search for particular repo and then do the shit
